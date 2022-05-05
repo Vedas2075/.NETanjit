7aebf59df3f895e279fb56c4d50ffff3d7143d2b
anjitsilwal10 <anjitsilwal.10@outlook.com>
2022-05-05 11:38:22 +0545

    may 5

---
+++ Classes/CSharpFundamentals/Collection.cs
@@ -0,0 +1,13 @@
+public class collection 
+{
+    byte[] ages = {23,45,34,34,23,56,76,76};
+    //List 
+    List <string> names = new();
+    void Test()
+        {
+            names.Add("anjit");
+            names.Remove("anjit");
+            var x = names.Contains("Ram");
+        }
+
+}
